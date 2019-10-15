0e99833783e5c27de720b03c591cb5381fcaee6e
Muyao <chenmuyao95@gmail.com>
2019-10-15 21:23:53 +0200

    correct script

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 
 sudo apt install zsh tmux xsel xclip
 
-git clone https://github.com/gpakosz/.tmux.git $HOME
-ln -s -f .tmux/.tmux.conf -t $HOME
-cp ./.tmux.conf.local $HOME
+git clone https://github.com/gpakosz/.tmux.git $HOME/.tmux
+git clone hln -s -f $HOME/.tmux/.tmux.conf -t $HOME
+git clone hcp ./.tmux.conf.local $HOME
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
