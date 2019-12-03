7258160333c605bf3d7cb0d3dd1b3b05bccfab50
muyao <chenmuyao95@gmail.com>
2019-12-03 16:16:35 +0100

    relative to definitive

--- script.sh
+++ script.sh
@@ -5,9 +5,10 @@ sudo apt install zsh tmux xsel xclip neovim git
 
 git clone https://github.com/gpakosz/.tmux.git $HOME/.tmux
 ln -s -f $HOME/.tmux/.tmux.conf -t $HOME
-ln -s -f ./.tmux.conf.local $HOME
+ln -s -f $PWD/.tmux.conf.local $HOME
+ln -s -f $PWD/.vimrc $HOME
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -s -f ./.zshrc $HOME
+ln -s -f $PWD/.zshrc $HOME
